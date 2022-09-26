acbde943787b88018fc87d3b92dd4cca1140a98a
sugarfreemedia <108233348+sugarfreemedia@users.noreply.github.com>
2022-09-26 12:42:32 -0700

    removed depracated RGBLIGHT_ANIMATIONS from config.h

--- keyboards/boardwalk/config.h
+++ keyboards/boardwalk/config.h
@@ -66,7 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // ws2812 options
 #define RGB_DI_PIN B7 // pin the DI on the ws2812 is hooked-up to
-#define RGBLIGHT_ANIMATIONS // run RGB animations
+// #define RGBLIGHT_ANIMATIONS // run RGB animations is deprecated
 #define RGBLED_NUM 14 // number of LEDs
 #define RGBLIGHT_HUE_STEP 12 // units to step when in/decreasing hue
 #define RGBLIGHT_SAT_STEP 25 // units to step when in/decresing saturation
